9f475a173732b85ba5a3e5e50a02107d2f99febb
KaranKathur06 <karan.business.acc04061@gmail.com>
2026-02-22 14:26:43 +0530

    git pages removed

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_PAGES === "true";
-
 const nextConfig = {
-  ...(isGithubPages
-    ? {
-        trailingSlash: true,
-        basePath: "/Metal-Hub",
-        assetPrefix: "/Metal-Hub/",
-      }
-    : {}),
   images: {
     unoptimized: true,
   },
